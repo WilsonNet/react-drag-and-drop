aaeb08412355cc8df7b06fbb05d97625c67d24cf
Wilson <wilson.neto@nerdweb.com.br>
2020-03-04 08:56:57 -0300

    Started project

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-drag-and-drop
+Drag and drop based on Tyler Potts tutorial on youtube
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="fexbox"></main>
+    </div>
+  );
+}
+
+export default App;
